296472ad4303f66d042cfba7f277af112d92ab4a
yuzhentao <yuzhentao263@qq.com>
2019-07-10 18:30:17 +0800

    no message

--- app/src/main/java/com/yuzhentao/ktvideo/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/ui/activity/SplashActivity.kt
@@ -2,13 +2,25 @@ package com.yuzhentao.ktvideo.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import com.yuzhentao.ktvideo.R
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+        initView()
+        anim()
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun anim() {
+
     }
 
 }
