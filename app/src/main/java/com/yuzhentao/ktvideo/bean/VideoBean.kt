081e17ba54c50f058bc03461d4d2b00cd42f9a80
yuzhentao <yuzhentao263@qq.com>
2019-07-12 18:23:49 +0800

    no message

---
+++ app/src/main/java/com/yuzhentao/ktvideo/bean/VideoBean.kt
@@ -0,0 +1,9 @@
+package com.yuzhentao.ktvideo.bean
+
+class VideoBean(var feed: String?, var title: String?, var description: String?,
+                var duration: Long?, var playUrl: String?, var category: String?,
+                var blurred: String?, var collect: Int?, var share: Int?, var reply: Int?, var time: Long) {
+
+
+
+}
