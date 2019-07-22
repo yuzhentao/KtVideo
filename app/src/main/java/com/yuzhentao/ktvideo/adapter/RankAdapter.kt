15ee2cd0ba11a26611e648d791d41e6b14d614ca
yuzhentao <yuzhentao263@qq.com>
2019-07-22 18:46:20 +0800

    热门界面1

--- app/src/main/java/com/yuzhentao/ktvideo/adapter/RankAdapter.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/adapter/RankAdapter.kt
@@ -1,7 +1,50 @@
 package com.yuzhentao.ktvideo.adapter
 
-class RankAdapter {
+import android.content.Context
+import android.graphics.Typeface
+import android.support.v7.widget.AppCompatImageView
+import android.support.v7.widget.AppCompatTextView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yuzhentao.ktvideo.R
+import com.yuzhentao.ktvideo.bean.HotBean
 
+class RankAdapter(context: Context?, beans: ArrayList<HotBean.Item.Data>) : RecyclerView.Adapter<RankAdapter.ViewHolder>() {
 
+    var context: Context? = null
+    var beans: ArrayList<HotBean.Item.Data>? = null
+    var inflater: LayoutInflater? = null
+
+    init {
+        this.context = context
+        this.beans = beans
+        this.inflater = LayoutInflater.from(context)
+    }
+
+    override fun getItemCount(): Int {
+        return beans?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(inflater!!.inflate(R.layout.item_rank, parent, false), context!!)
+    }
+
+    override fun onBindViewHolder(parent: ViewHolder, viewType: Int) {
+
+    }
+
+    class ViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
+
+        var iv: AppCompatImageView = itemView.findViewById(R.id.iv) as AppCompatImageView
+        var tvTitle: AppCompatTextView = itemView.findViewById(R.id.tv_title) as AppCompatTextView
+        var tvTime: AppCompatTextView = itemView.findViewById(R.id.tv_time) as AppCompatTextView
+
+        init {
+            tvTitle.typeface = Typeface.createFromAsset(context.assets, "fonts/FZLanTingHeiS-L-GB-Regular.TTF")
+        }
+
+    }
 
 }
--- app/src/main/java/com/yuzhentao/ktvideo/ui/fragment/HotFragment.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/ui/fragment/HotFragment.kt
@@ -1,15 +1,39 @@
 package com.yuzhentao.ktvideo.ui.fragment
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.yuzhentao.ktvideo.R
 
 class HotFragment : BaseFragment() {
+
+    private var tabs = mutableListOf("周排行", "月排行", "总排行")
+    lateinit var fragments: ArrayList<Fragment>
+    var strategy = arrayOf("weekly", "monthly", "historical")
 
     override fun getLayoutResources(): Int {
         return R.layout.fragment_hot
     }
 
     override fun initView() {
+        val weekFragment = RankingFragment()
+        val weekBundle = Bundle()
+        weekBundle.putString("strategy", strategy[0])
+        weekFragment.arguments = weekBundle
 
+        val monthFragment = RankingFragment()
+        val monthBundle = Bundle()
+        monthBundle.putString("strategy", strategy[0])
+        monthFragment.arguments = monthBundle
+
+        val allFragment = RankingFragment()
+        val allBundle = Bundle()
+        allBundle.putString("strategy", strategy[0])
+        allFragment.arguments = allBundle
+
+        fragments = ArrayList()
+        fragments.add(weekFragment)
+        fragments.add(monthFragment)
+        fragments.add(allFragment)
     }
 
     override fun onFragmentVisibleChange(b: Boolean) {
