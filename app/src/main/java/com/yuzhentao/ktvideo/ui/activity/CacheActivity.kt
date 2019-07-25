30c9ec19cd439c63f11b736979b152cf4928b16d
yuzhentao <yuzhentao263@qq.com>
2019-07-25 13:20:18 +0800

    缓存1

--- app/src/main/java/com/yuzhentao/ktvideo/ui/activity/CacheActivity.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/ui/activity/CacheActivity.kt
@@ -2,16 +2,26 @@ package com.yuzhentao.ktvideo.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.yuzhentao.ktvideo.R
 
 /**
  * 我的缓存
  */
-class CacheActivity : AppCompatActivity() {
+class CacheActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cache)
+        initView()
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+    private fun initView() {
+
     }
 
 }
