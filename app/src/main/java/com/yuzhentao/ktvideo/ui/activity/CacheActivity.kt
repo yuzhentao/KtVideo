8ab1190e211321ff77efa68909b7b259fafbcd3c
yuzhentao <yuzhentao263@qq.com>
2019-07-26 16:41:41 +0800

    使用realm来进行数据的获取

--- app/src/main/java/com/yuzhentao/ktvideo/ui/activity/CacheActivity.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/ui/activity/CacheActivity.kt
@@ -3,7 +3,6 @@ package com.yuzhentao.ktvideo.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
@@ -11,11 +10,8 @@ import com.gyf.barlibrary.ImmersionBar
 import com.yuzhentao.ktvideo.R
 import com.yuzhentao.ktvideo.adapter.CacheAdapter
 import com.yuzhentao.ktvideo.bean.VideoBean
+import com.yuzhentao.ktvideo.db.VideoDbManager
 import com.yuzhentao.ktvideo.interfaces.OnItemClickListener
-import com.yuzhentao.ktvideo.util.ObjectSaveUtils
-import com.yuzhentao.ktvideo.util.SPUtils
-import com.yuzhentao.ktvideo.util.normalSchedulers
-import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_cache.*
 
@@ -33,16 +29,20 @@ class CacheActivity : AppCompatActivity(), View.OnClickListener {
 
     private var disposable: Disposable? = null
 
+    private lateinit var dbManager: VideoDbManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cache)
+        dbManager = VideoDbManager()
         ImmersionBar.with(activity).transparentBar().barAlpha(0.3F).fitsSystemWindows(true).init()
         initView()
         initData()
     }
 
     override fun onDestroy() {
-        disposable.let {
+        dbManager.close()
+        disposable?.let {
             if (!disposable!!.isDisposed) {
                 disposable!!.dispose()
             }
@@ -86,39 +86,19 @@ class CacheActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initData() {
-        val observable: Observable<ArrayList<VideoBean>> = Observable.create { emitter ->
-            val beans = ArrayList<VideoBean>()
-            val count = SPUtils.getInstance(context, "downloads").getInt("count")
-            var num = 1
-            while (num <= count) {
-                var bean: VideoBean
-                if (ObjectSaveUtils.getValue(context, "download$num") == null) {
-                    continue
-                } else {
-                    bean = ObjectSaveUtils.getValue(context, "download$num") as VideoBean
-                }
-                beans.add(bean)
-                num++
+        dbManager.findAll(VideoBean::class.java)?.let {
+            if (beans.size > 0) {
+                beans.clear()
             }
-            emitter.onNext(beans)
-        }
-        disposable = observable.normalSchedulers().subscribe { tempBeans: ArrayList<VideoBean>? ->
-            tempBeans?.let {
-                if (tempBeans.size > 0) {
-                    rv.visibility = View.VISIBLE
-                    tv_hint.visibility = View.GONE
-                } else {
-                    rv.visibility = View.GONE
-                    tv_hint.visibility = View.VISIBLE
-                }
-                if (beans.size > 0) {
-                    beans.clear()
-                }
-                tempBeans.forEach {
-                    beans.add(it)
-                }
-                adapter.notifyDataSetChanged()
+            beans.addAll(it)
+            if (beans.size > 0) {
+                rv.visibility = View.VISIBLE
+                tv_hint.visibility = View.GONE
+            } else {
+                rv.visibility = View.GONE
+                tv_hint.visibility = View.VISIBLE
             }
+            adapter.notifyDataSetChanged()
         }
     }
 
