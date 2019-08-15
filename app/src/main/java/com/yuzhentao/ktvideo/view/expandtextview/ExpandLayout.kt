fc366e5a57fd2256cd74abead9d64a41f7bb22ae
yuzhentao <yuzhentao263@qq.com>
2019-08-15 11:08:24 +0800

    优化ExpandLayout1

--- app/src/main/java/com/yuzhentao/ktvideo/view/expandtextview/ExpandLayout.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/view/expandtextview/ExpandLayout.kt
@@ -1,84 +1,82 @@
 package com.yuzhentao.ktvideo.view.expandtextview
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import com.yuzhentao.ktvideo.R
+import com.yuzhentao.ktvideo.util.DimenUtil
 import kotlinx.android.synthetic.main.layout_expand_view.view.*
 
-/**
- *  Created by lingdong on 2018/5/6.
- *
- *
- **/
-class ExpandLayout @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+class ExpandLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
 
-    private var mListener: OnExpandListener? = null
+    private var listener: OnExpandListener? = null
 
     private var maxCollapsedLines = 3
-    private var contentTextSize = 18f
+    private var contentTextSize = 18F
     private var contentTextColor = 0
     private var expandText = ""
     private var collapseText = ""
-    private var expandCollapseTextSize = 18f
+    private var expandCollapseTextSize = 18F
     private var expandCollapseTextColor = 0
     private var expandCollapseTextGravity = 0
+    private var expandCollapseTextLayoutGravity = 0
     private var ellipsizeText = "..."
-    private var middlePadding = 0f
+    private var middlePadding = 0F
 
     init {
-        initAttrs(context, attrs, defStyleAttr)
+        initAttrs(context, attrs)
         orientation = VERTICAL
         LayoutInflater.from(context).inflate(R.layout.layout_expand_view, this)
         etv_content.setMaxLineCount(maxCollapsedLines)
-        etv_content.textSize = DensityUtil.px2sp(context, contentTextSize)
+        etv_content.textSize = DimenUtil.px2sp(context, contentTextSize).toFloat()
         etv_content.setTextColor(contentTextColor)
         etv_content.setEllipsizeText(ellipsizeText)
-
-        var lp = tv_tip.layoutParams as LinearLayout.LayoutParams
+        val lp = tv_tip.layoutParams as LayoutParams
         lp.topMargin = middlePadding.toInt()
+        lp.gravity = when (expandCollapseTextLayoutGravity) {
+            0 -> Gravity.START
+            1 -> Gravity.CENTER
+            2 -> Gravity.END
+            else -> Gravity.START
+        }
         tv_tip.layoutParams = lp
-
-        tv_tip.textSize = DensityUtil.px2sp(context, expandCollapseTextSize)
+        tv_tip.textSize = DimenUtil.px2sp(context, expandCollapseTextSize).toFloat()
         tv_tip.setTextColor(expandCollapseTextColor)
         tv_tip.gravity = when (expandCollapseTextGravity) {
-            0 -> Gravity.LEFT
+            0 -> Gravity.START
             1 -> Gravity.CENTER
-            2 -> Gravity.RIGHT
-            else -> Gravity.LEFT
+            2 -> Gravity.END
+            else -> Gravity.START
         }
-
         etv_content.requestLayout()
         tv_tip.requestLayout()
-
     }
 
-    private fun initAttrs(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
-        var ta = context.obtainStyledAttributes(attrs, R.styleable.ExpandLayout)
+    private fun initAttrs(context: Context, attrs: AttributeSet?) {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.ExpandLayout)
         maxCollapsedLines = ta.getInt(R.styleable.ExpandLayout_maxCollapsedLines, 3)
-        contentTextSize = ta.getDimension(R.styleable.ExpandLayout_contentTextSize, DensityUtil.sp2px(context, 18f).toFloat())
-        contentTextColor = ta.getColor(R.styleable.ExpandLayout_contentTextColor, resources.getColor(R.color.text_black))
-        expandText = if (ta.getString(R.styleable.ExpandLayout_expandText).isNullOrEmpty()) "全文" else ta.getString(R.styleable.ExpandLayout_expandText)
-        collapseText = if (ta.getString(R.styleable.ExpandLayout_collapseText).isNullOrEmpty()) "收起" else ta.getString(R.styleable.ExpandLayout_collapseText)
-        expandCollapseTextSize = ta.getDimension(R.styleable.ExpandLayout_expandCollapseTextSize, DensityUtil.sp2px(context, 18f).toFloat())
-        expandCollapseTextColor = ta.getColor(R.styleable.ExpandLayout_expandCollapseTextColor, resources.getColor(R.color.text_blue))
-        expandCollapseTextGravity = ta.getColor(R.styleable.ExpandLayout_expandCollapseTextGravity, 0)
-        ellipsizeText = if (ta.getString(R.styleable.ExpandLayout_ellipsizeText).isNullOrEmpty()) "..." else ta.getString(R.styleable.ExpandLayout_ellipsizeText)
+        contentTextSize = ta.getDimension(R.styleable.ExpandLayout_contentTextSize, DimenUtil.sp2px(context, 18F).toFloat())
+        contentTextColor = ta.getColor(R.styleable.ExpandLayout_contentTextColor, ContextCompat.getColor(context, R.color.text_black))
+        expandText = if (ta.getString(R.styleable.ExpandLayout_expandText).isNullOrEmpty()) "全文" else ta.getString(R.styleable.ExpandLayout_expandText)!!
+        collapseText = if (ta.getString(R.styleable.ExpandLayout_collapseText).isNullOrEmpty()) "收起" else ta.getString(R.styleable.ExpandLayout_collapseText)!!
+        expandCollapseTextSize = ta.getDimension(R.styleable.ExpandLayout_expandCollapseTextSize, DimenUtil.sp2px(context, 18F).toFloat())
+        expandCollapseTextColor = ta.getColor(R.styleable.ExpandLayout_expandCollapseTextColor, ContextCompat.getColor(context, R.color.text_blue))
+        expandCollapseTextGravity = ta.getInt(R.styleable.ExpandLayout_expandCollapseTextGravity, 0)
+        expandCollapseTextLayoutGravity = ta.getInt(R.styleable.ExpandLayout_expandCollapseTextLayoutGravity, 0)
+        ellipsizeText = if (ta.getString(R.styleable.ExpandLayout_ellipsizeText).isNullOrEmpty()) "..." else ta.getString(R.styleable.ExpandLayout_ellipsizeText)!!
         middlePadding = ta.getDimension(R.styleable.ExpandLayout_middlePadding, 0f)
         ta.recycle()
     }
 
-
     fun setText(text: String, expand: Boolean, listener: OnExpandListener) {
-        mListener = listener
-        ll_expand_view.setOnClickListener({
-            mListener?.expandChange()
-        })
+        this.listener = listener
+        ll_expand_view.setOnClickListener {
+            this.listener?.expandChange()
+        }
         etv_content.setChanged(expand)
         etv_content.setText(text, expand, object : ExpandTextView.Callback {
             override fun onExpand() {
@@ -94,12 +92,13 @@ class ExpandLayout @JvmOverloads constructor(
             override fun onLoss() {
                 tv_tip.visibility = View.GONE
             }
-
         })
     }
 
     interface OnExpandListener {
+
         fun expandChange()
+
     }
 
 }
--- app/src/main/java/com/yuzhentao/ktvideo/view/expandtextview/ExpandTextView.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/view/expandtextview/ExpandTextView.kt
@@ -7,11 +7,6 @@ import android.text.Layout
 import android.text.StaticLayout
 import android.util.AttributeSet
 
-/**
- *  Created by lingdong on 2018/5/6.
- *
- *
- **/
 class ExpandTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     /**
@@ -38,7 +33,7 @@ class ExpandTextView @JvmOverloads constructor(context: Context, attrs: Attribut
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        var sl = StaticLayout(mText, paint, measuredWidth - paddingLeft - paddingRight, Layout.Alignment.ALIGN_CENTER, 1f, 0f, true)
+        val sl = StaticLayout(mText, paint, measuredWidth - paddingLeft - paddingRight, Layout.Alignment.ALIGN_CENTER, 1F, 0F, true)
         /**
          * 总计行数
          */
@@ -47,57 +42,47 @@ class ExpandTextView @JvmOverloads constructor(context: Context, attrs: Attribut
             when (mExpanded) {
                 true -> {
                     mCallback?.onExpand()
-                    setText(mText)
-
-
+                    text = mText
                 }
                 false -> {
                     mCallback?.onCollapse()
 
                     lineCount = maxLineCount
                     //省略文字的宽度
-                    var dotWidth = paint.measureText(ellipsizeText)
-                    var start = sl.getLineStart(lineCount - 1)
-                    var end = sl.getLineEnd(lineCount - 1)
-                    var lineText = mText.substring(start, end)
-
+                    val dotWidth = paint.measureText(ellipsizeText)
+                    val start = sl.getLineStart(lineCount - 1)
+                    val end = sl.getLineEnd(lineCount - 1)
+                    val lineText = mText.substring(start, end)
                     var endIndex = 0
                     for (i in lineText.length - 1 downTo 0) {
-                        var str = lineText.substring(i, lineText.length)
+                        val str = lineText.substring(i, lineText.length)
                         if (paint.measureText(str) >= dotWidth) {
                             endIndex = i
                             break
                         }
                     }
-
-                    var newEndLineText = lineText.substring(0, endIndex) + ellipsizeText
-                    setText(mText.substring(0, start) + newEndLineText)
-
-
+                    val newEndLineText = lineText.substring(0, endIndex) + ellipsizeText
+                    text = mText.substring(0, start) + newEndLineText
                 }
             }
         } else {
             mCallback?.onLoss()
-            setText(mText)
+            text = mText
         }
-
         var lineHeight = 0
         for (i in 0 until lineCount) {
-            var lienBound = Rect()
+            val lienBound = Rect()
             sl.getLineBounds(i, lienBound)
             lineHeight += lienBound.height()
         }
-
         lineHeight += paddingTop + paddingBottom
         setMeasuredDimension(measuredWidth, lineHeight)
-
     }
 
     fun setText(text: String, expanded: Boolean, callback: Callback) {
         mText = text
         mExpanded = expanded
         mCallback = callback
-
         setText(text)
     }
 
@@ -107,9 +92,13 @@ class ExpandTextView @JvmOverloads constructor(context: Context, attrs: Attribut
     }
 
     interface Callback {
+
         fun onExpand()
+
         fun onCollapse()
+
         fun onLoss()
+
     }
 
 }
