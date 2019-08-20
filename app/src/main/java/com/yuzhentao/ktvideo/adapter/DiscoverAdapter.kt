4e212bf413143fbfc23fde090c994cead17c91f8
yuzhentao <yuzhentao263@qq.com>
2019-08-20 16:46:39 +0800

    过度绘制问题

--- app/src/main/java/com/yuzhentao/ktvideo/adapter/DiscoverAdapter.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/adapter/DiscoverAdapter.kt
@@ -2,10 +2,12 @@ package com.yuzhentao.ktvideo.adapter
 
 import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
+import android.view.MotionEvent
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.yuzhentao.ktvideo.R
 import com.yuzhentao.ktvideo.bean.DiscoverBean
+import com.yuzhentao.ktvideo.extension.color
 import com.yuzhentao.ktvideo.util.ImageUtil
 
 class DiscoverAdapter(data: MutableList<DiscoverBean.Item.Data>?) : BaseQuickAdapter<DiscoverBean.Item.Data, BaseViewHolder>(R.layout.item_discover, data) {
@@ -13,10 +15,24 @@ class DiscoverAdapter(data: MutableList<DiscoverBean.Item.Data>?) : BaseQuickAda
     override fun convert(helper: BaseViewHolder?, item: DiscoverBean.Item.Data?) {
         helper?.let {
             item?.let {
+                val iv = helper.getView<AppCompatImageView>(R.id.iv)
                 item.image?.let {
-                    ImageUtil.show(mContext!!, helper.getView<AppCompatImageView>(R.id.iv_photo), item.image)
+                    ImageUtil.show(mContext!!, iv, item.image)
                 }
+                iv.setColorFilter(mContext.color(R.color.black_25))
                 helper.getView<AppCompatTextView>(R.id.tv).text = item.title
+                helper.itemView.setOnTouchListener { v, event ->
+                    when (event?.action) {
+                        MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                            iv.setColorFilter(mContext.color(R.color.black_10))
+                        }
+                        MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_OUTSIDE -> {
+                            iv.setColorFilter(mContext.color(R.color.black_25))
+                        }
+                        else -> v?.performClick()
+                    }
+                    false
+                }
             }
         }
     }
--- app/src/main/java/com/yuzhentao/ktvideo/adapter/RankingSubAdapter.kt
+++ app/src/main/java/com/yuzhentao/ktvideo/adapter/RankingSubAdapter.kt
@@ -2,21 +2,25 @@ package com.yuzhentao.ktvideo.adapter
 
 import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
+import android.view.MotionEvent
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.yuzhentao.ktvideo.R
 import com.yuzhentao.ktvideo.bean.RankingSubBean
+import com.yuzhentao.ktvideo.extension.color
 import com.yuzhentao.ktvideo.util.ImageUtil
 
-class RankingSubAdapter(data: MutableList<RankingSubBean.Item.Data.Content.DataX>?) : BaseQuickAdapter<RankingSubBean.Item.Data.Content.DataX, BaseViewHolder>(R.layout.item_ranking, data) {
+class RankingSubAdapter(data: MutableList<RankingSubBean.Item.Data.Content.DataX>?) : BaseQuickAdapter<RankingSubBean.Item.Data.Content.DataX, BaseViewHolder>(R.layout.item_ranking_sub, data) {
 
     override fun convert(helper: BaseViewHolder?, item: RankingSubBean.Item.Data.Content.DataX?) {
         helper?.let {
             item?.let {
+                val iv = helper.getView<AppCompatImageView>(R.id.iv)
                 val img = item.cover?.feed
                 img?.let {
-                    ImageUtil.show(mContext!!, helper.getView<AppCompatImageView>(R.id.iv), img)
+                    ImageUtil.show(mContext!!, iv, img)
                 }
+                iv.setColorFilter(mContext.color(R.color.black_25))
                 val title = item.title
                 helper.getView<AppCompatTextView>(R.id.tv_title).text = title
                 val category = item.category
@@ -37,6 +41,18 @@ class RankingSubAdapter(data: MutableList<RankingSubBean.Item.Data.Content.DataX
                 }
                 val content = "$category / $realMinute'$realSecond''"
                 helper.getView<AppCompatTextView>(R.id.tv_time).text = content
+                helper.itemView.setOnTouchListener { v, event ->
+                    when (event?.action) {
+                        MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                            iv.setColorFilter(mContext.color(R.color.black_10))
+                        }
+                        MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_OUTSIDE -> {
+                            iv.setColorFilter(mContext.color(R.color.black_25))
+                        }
+                        else -> v?.performClick()
+                    }
+                    false
+                }
             }
         }
     }
